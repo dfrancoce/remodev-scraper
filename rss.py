d3771d796ac42d2b652c6b49c6cc138eb14e6884
dfc <dfrancoce@protonmail.com>
2018-07-24 10:53:25 +0200

    Added weworkremotely rss support

--- rss.py
+++ rss.py
@@ -3,8 +3,8 @@ from models import JobOffer
 
 class Stackoverflow(object):
     def parse(self):
-        stackoverflow_rss_url = "https://stackoverflow.com/jobs/feed?l=Remote&u=Km&d=20"
-        feed = feedparser.parse(stackoverflow_rss_url)
+        url = "https://stackoverflow.com/jobs/feed?l=Remote&u=Km&d=20"
+        feed = feedparser.parse(url)
 
         jobOffers = []
         for entry in feed.entries:
@@ -20,3 +20,15 @@ class Stackoverflow(object):
             extractedTags.append(tag["term"])
 
         return extractedTags
+
+class WeWorkRemotely(object):
+    def parse(self):
+        url = "https://weworkremotely.com/categories/remote-programming-jobs.rss"
+        feed = feedparser.parse(url)
+
+        jobOffers = []
+        for entry in feed.entries:
+            jobOffer = JobOffer(entry["link"], entry["title"], entry["summary"], entry["title"].split(":")[0], entry["published"], "")
+            jobOffers.append(jobOffer)
+        
+        return jobOffers
