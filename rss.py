561ab1eb141405f0db03ac7a0ac72d55c885e45c
dfc <dfrancoce@protonmail.com>
2018-07-24 12:48:25 +0200

    Refactoring

--- rss.py
+++ rss.py
@@ -2,33 +2,40 @@ import feedparser
 from models import JobOffer
 
 class Stackoverflow(object):
+    """ This class parses the response of the search for remote jobs in stackoverflow """
+    
     def parse(self):
+        """ Parses the results of the stackoverflow remote jobs search into a list of JobOffer objects """
         url = "https://stackoverflow.com/jobs/feed?l=Remote&u=Km&d=20"
         feed = feedparser.parse(url)
 
-        jobOffers = []
+        job_offers = []
         for entry in feed.entries:
             tags = self.extractTags(entry["tags"])
-            jobOffer = JobOffer(entry["link"], entry["title"], entry["description"], entry["author"], entry["updated"], tags)
-            jobOffers.append(jobOffer)
+            job_offer = JobOffer(entry["link"], entry["title"], entry["description"], entry["author"], entry["updated"], tags)
+            job_offers.append(job_offer)
         
-        return jobOffers
+        return job_offers
 
     def extractTags(self, tags):
-        extractedTags = []
+        extracted_tags = []
         for tag in tags:
-            extractedTags.append(tag["term"])
+            extracted_tags.append(tag["term"])
 
-        return extractedTags
+        return extracted_tags
 
 class WeWorkRemotely(object):
+    """ This class parses the response of the weworkremotely remote programming jobs rss feed """
+
     def parse(self):
+        """ Parses the results of the weworkremotely remote programming jobs into a list of JobOffer objects """
+
         url = "https://weworkremotely.com/categories/remote-programming-jobs.rss"
         feed = feedparser.parse(url)
 
-        jobOffers = []
+        job_offers = []
         for entry in feed.entries:
-            jobOffer = JobOffer(entry["link"], entry["title"], entry["summary"], entry["title"].split(":")[0], entry["published"], "")
-            jobOffers.append(jobOffer)
+            job_offer = JobOffer(entry["link"], entry["title"], entry["summary"], entry["title"].split(":")[0], entry["published"], "")
+            job_offers.append(job_offer)
         
-        return jobOffers
+        return job_offers
