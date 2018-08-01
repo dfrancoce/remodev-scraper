2e3a3ce2e6a3b31bda2ac0ce0a3eaa89ea307872
dfc <dfrancoce@protonmail.com>
2018-08-01 15:46:07 +0200

    Created main to run all the collectors

--- main.py
+++ main.py
@@ -1,47 +1,65 @@
 import scrapy
 
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy import signals
+from twisted.internet import reactor, defer
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 from scraper.jobbie.spiders.europe_remotely_spider import EuropeRemotelySpider
 from scraper.jobbie.spiders.remotelist_io_spider import RemoteListIoSpider
+from api import Github
+from api import RemoteOk
+from rss import Stackoverflow
+from rss import WeWorkRemotely
+from models import JobOffer
 
 results = []
 
 def main():
-    runSpiders()
-    
-def runSpiders():
-    # set up spiders
-    europe_remotely_spider = EuropeRemotelySpider()
-    remotelis_io_spider = RemoteListIoSpider()
+    """ Runs collectors"""
+    runSpidersCollector()
+    runApisCollector()
+    runRsssCollector()
 
-    # set up settings
+def runApisCollector():
+    """ Runs the API collectors and saves the collected job offers in the results object"""
+    github = Github()
+    remoteOk = RemoteOk()
+    
+    for item in github.get():
+        results.append(item)
+
+    for item in remoteOk.get():
+        results.append(item)
+
+def runRsssCollector():
+    """ Runs the rss collectors and saves the collected job offers in the results object"""
+    stackOverflow = Stackoverflow()
+    weWorkRemotely = WeWorkRemotely()
+
+    for item in stackOverflow.parse():
+        results.append(item)
+
+    for item in weWorkRemotely.parse():
+        results.append(item)
+    
+def runSpidersCollector():
+    """ Runs the spider collectors and saves the collected job offers in the results object"""
     settings = get_project_settings()
     settings['ITEM_PIPELINES'] = {'__main__.SpidersPipeline': 1}
-
-    # set up crawlers
-    europe_remotely_crawler = Crawler(europe_remotely_spider, settings)
-    europe_remotely_crawler.signals.connect(spiderClosed, signal=signals.spider_closed)
-
-    #remotelis_io_crawler = Crawler(remotelis_io_spider, settings)
-    #remotelis_io_crawler.signals.connect(spiderClosed, signal=signals.spider_closed)
-
-    # start crawling
-    europe_remotely_crawler.crawl()
-    #remotelis_io_crawler.crawl()
+    runner = CrawlerRunner(settings)
+    crawl(runner)
     reactor.run()
 
-def spiderClosed(spider):
-    for result in results:
-        print(result)
+@defer.inlineCallbacks
+def crawl(runner):
+    yield runner.crawl(EuropeRemotelySpider)
+    yield runner.crawl(RemoteListIoSpider)
 
     reactor.stop()
 
 class SpidersPipeline(object):
+    """ Pipeline to transform spiders results into JobOffer objects and save them into the results object"""
     def process_item(self, item, spider):
-        results.append(dict(item))
+        results.append(JobOffer(item['url'], item['position'], "", item['company'], item['date'], item['tags']))
 
 if __name__ == '__main__':
     main()
--- scraper/jobbie/spiders/remotelist_io_spider.py
+++ scraper/jobbie/spiders/remotelist_io_spider.py
@@ -2,11 +2,12 @@ import scrapy
 
 from ..items import Job
 
+DOMAIN = "https://remotelist.io"
+
 class RemoteListIoSpider(scrapy.Spider):
     """ This class crawls the remote jobs in remotelist """
 
     name = "remotelistio"
-
     def start_requests(self):
         urls = [
             'https://remotelist.io/'
@@ -20,7 +21,7 @@ class RemoteListIoSpider(scrapy.Spider):
         
         for row in response.css("tr.row"):
             job = Job()
-            job["url"] = row.css("td")[0].css("div::attr(data-url)").extract_first()
+            job["url"] = DOMAIN + row.css("td")[0].css("div::attr(data-url)").extract_first()
             job["position"] = row.css("td")[0].css("strong::text").extract_first()
             job["company"] = row.css("td")[1].css("a::text").extract_first()
             job["tags"] = row.css("td")[2].css("a::text").extract()
